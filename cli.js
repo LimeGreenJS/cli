a9bfc92f08f97f34be0426124bca5b4e8cf69992
daishi <daishi@axlight.com>
2017-10-23 22:49:39 +0900

    v0.2.0

--- cli.js
+++ cli.js
@@ -7,61 +7,73 @@ const koaWebpack = require('koa-webpack');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-program
-  .version(version)
-  .usage('[options]')
-  .parse(process.argv);
-
-const app = new Koa();
-
-const config = {
-  entry: [
-    './index.js',
-    'webpack-hot-middleware/client',
-  ],
-  output: {
-    filename: '[hash]-index.js',
-    publicPath: '/',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: './index.html',
-    }),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NamedModulesPlugin(),
-    new webpack.NoEmitOnErrorsPlugin(),
-  ],
-  module: {
-    rules: [{
-      test: /\.jsx?$/,
-      exclude: /node_modules/,
-      use: [{
-        loader: 'babel-loader',
-        options: {
-          presets: [
-            ['env', { modules: false }],
-            'react',
-          ],
-          plugins: [
-            'transform-object-rest-spread',
-          ],
-        },
+const runDev = ({ port = 8080 }) => {
+  const app = new Koa();
+  const config = {
+    entry: [
+      './index.js',
+      'webpack-hot-middleware/client',
+    ],
+    output: {
+      filename: '[hash]-index.js',
+      publicPath: '/',
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: './index.html',
+      }),
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.NamedModulesPlugin(),
+      new webpack.NoEmitOnErrorsPlugin(),
+    ],
+    module: {
+      rules: [{
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['env', { modules: false }],
+              'react',
+            ],
+            plugins: [
+              'transform-object-rest-spread',
+            ],
+          },
+        }],
+      }, {
+        test: /\.(png|gif|jpg|svg)$/,
+        use: [{
+          loader: 'file-loader',
+          options: { name: '[hash]-[name].[ext]' },
+        }],
       }],
-    }, {
-      test: /\.(png|gif|jpg|svg)$/,
-      use: [{
-        loader: 'file-loader',
-        options: { name: '[hash]-[name].[ext]' },
-      }],
-    }],
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+  };
+  app.use(koaWebpack({ config }));
+  app.listen(port);
+  console.log('Listening on', port);
 };
-app.use(koaWebpack({ config }));
 
-const port = process.env.PORT || 8080;
-app.listen(port);
-console.log('Listening on', port);
+program
+  .version(version);
+
+program
+  .command('dev')
+  .description('run dev server')
+  .option('-p, --port [port]', 'Port number, defaults to 8080')
+  .action((options) => {
+    program.commandFound = true;
+    runDev(options);
+  });
+
+program.parse(process.argv);
+
+if (!program.commandFound) {
+  program.help();
+}
